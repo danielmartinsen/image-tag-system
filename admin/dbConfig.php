3239d28e1660a5e17613b0c183313a92a54c09bd
Daniel Martinsen <daniel.martinsen@online.no>
2020-12-31 21:09:37 +0100

    Update dbConfig.php

--- admin/dbConfig.php
+++ admin/dbConfig.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$dbHost     = "ungias.com.mysql";
-$dbUsername = "ungias_com_img";
-$dbPassword = "ungias_com_img";
-$dbName     = "ungias_com_img";
+$dbHost     = "";
+$dbUsername = "";
+$dbPassword = "";
+$dbName     = "";
 
 // Create database connection
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
